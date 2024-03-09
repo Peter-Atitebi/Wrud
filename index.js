b570353c69f95f07dd1fe9c78e9ac1ba15b5b471
Atitebi Oyinkansola Peter <132858902+Peter-Atitebi@users.noreply.github.com>
2024-03-09 05:50:14 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-import { initializeApp } from "firebase/app";
+import { error } from "console";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  collection,
-  onSnapshot,
-} from "firebase/firestore";
+  getDatabase,
+  ref,
+  child,
+  get,
+  set,
+  update,
+  remove,
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA0OQ4Uera27uMO8YB95HYf2roCOMrUJQs",
   authDomain: "crud-ee07e.firebaseapp.com",
+  databaseURL: "https://crud-ee07e-default-rtdb.firebaseio.com",
   projectId: "crud-ee07e",
   storageBucket: "crud-ee07e.appspot.com",
   messagingSenderId: "666520724549",
@@ -18,27 +22,82 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const accts = collection(db, "items");
-const db = getFirestore(app);
 
-const accounts = document.getElementById(accounts);
-const form = document.getElementById(formm);
+const db = getDatabase();
 
-form.addEventListener("submit", async function (e) {
-  e.preventDefault();
+let favouriteFood = document.getElementById("favourite_food");
+let Username = document.getElementById("username");
+let Password = document.getElementById("password");
+let Email = document.getElementById("email");
 
-  const email = document.getElementById("email").value;
+let CrtBtn = document.getElementById("CrtBtn");
+let RetBtn = document.getElementById("RetBtn");
+let UpdBtn = document.getElementById("UpdBtn");
+let DelBtn = document.getElementById("DelBtn");
 
-  await addDoc(accts, { Email: email });
+function AddData() {
+  set(ref(db, `User/` + Username.value), {
+    email: Email.value,
+    passsword: Password.value,
+    username: Username.value,
+    favouriteFood: favouriteFood.value,
+  })
+    .then(() => {
+      alert("Successfully added data");
+    })
+    .catch((error) => {
+      alert("Unsuccessful");
+      console.log(error);
+    });
+}
 
-  document.getElementById("email").value = "";
-});
+function RetData() {
+  const dbRef = ref(db);
 
-onSnapshot(accts, (snapshot) => {
-  snapshot.forEach((doc) => {
-    const data = doc.data();
-    let h3 = document.createElement("h3");
-    h3.textContent = data.email;
-    accounts.innerHTML += h3;
-  });
-});
+  get(child(dbRef, `User/` + Username.value))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        Email.value = snapshot.val().email;
+        Password.value = snapshot.val().passsword;
+        Username.value = snapshot.val().username;
+        favouriteFood.value = snapshot.val().favouriteFood;
+      } else {
+        alert("NULL");
+      }
+    })
+    .catch((error) => {
+      alert("Unsuccessful");
+      console.log(error);
+    });
+}
+
+function UpdData() {
+  update(ref(db, `User/` + Username.value), {
+    email: Email.value,
+    passsword: Password.value,
+    favouriteFood: favouriteFood.value,
+  })
+    .then(() => {
+      alert("Successfully updated data");
+    })
+    .catch((error) => {
+      alert("Unsuccessful");
+      console.log(error);
+    });
+}
+
+function DelData() {
+  remove(ref(db, `User/` + Username.value))
+    .then(() => {
+      alert("Successfully deleted data");
+    })
+    .catch((error) => {
+      alert("Unsuccessful");
+      console.log(error);
+    });
+}
+
+CrtBtn.addEventListener("click", AddData);
+RetBtn.addEventListener("click", RetData);
+UpdBtn.addEventListener("click", UpdData);
+DelBtn.addEventListener("click", DelData);
